0393ff2d2fb5c92eb0adaac32a09162ff9080af1
benwa <btellier@linagora.com>
2015-02-06 00:06:50 +0100

    Geo bounding box filter

---
+++ src/main/java/tellier/es/dsl/query/builder/Utilities/DSLGeoBox.java
@@ -0,0 +1,78 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package tellier.es.dsl.query.builder.Utilities;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * Represents a geo box
+ */
+public class DSLGeoBox {
+    
+    private static final String TOP_LEFT = "top_left";
+    private static final String BOTTOM_RIGHT = "bottom_right";
+    private static final String TOP = "top";
+    private static final String RIGHT = "right";
+    private static final String LEFT = "left";
+    private static final String BOTTOM = "bottom";
+
+    private Mode mode;
+    
+    private DSLGeoPoint topLeft;
+    private DSLGeoPoint bottomRight;
+    
+    private double top;
+    private double left;
+    private double bottom;
+    private double right;
+    
+    enum Mode {
+        Points,
+        Vertices
+    }
+
+    public DSLGeoBox(double top, double left, double bottom, double right) {
+        this.top = top;
+        this.left = left;
+        this.bottom = bottom;
+        this.right = right;
+        this.mode = Mode.Vertices;
+    }
+
+    public DSLGeoBox(DSLGeoPoint topLeft, DSLGeoPoint bottomRight) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+        this.mode = Mode.Points;
+    }
+    
+    public JsonObject getBoxAsJsonObject() {
+        JsonObject result = new JsonObject();
+        if(mode == Mode.Points) {
+            result.add(TOP_LEFT, topLeft.getPointAsJsonElement());
+            result.add(BOTTOM_RIGHT, bottomRight.getPointAsJsonElement());
+        } else {
+            result.add(TOP, new JsonPrimitive(top));
+            result.add(BOTTOM, new JsonPrimitive(bottom));
+            result.add(RIGHT, new JsonPrimitive(right));
+            result.add(LEFT, new JsonPrimitive(left));
+        }
+        return result;
+    }
+}
---
+++ src/main/java/tellier/es/dsl/query/builder/Utilities/DSLGeoPoint.java
@@ -0,0 +1,79 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package tellier.es.dsl.query.builder.Utilities;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * Represents a geo localized point for Geo filters
+ */
+public class DSLGeoPoint {
+    
+    private static final String LAT = "lat";
+    private static final String LON = "lon";
+    
+    public enum Mode {
+        LatLon,
+        Array,
+        Properties
+    }
+    
+    private String geoHash;
+    private double longitude;
+    private double latitude;
+    private Mode mode;
+    private boolean isGeoHash = false;
+    
+    public DSLGeoPoint(String geoHash) {
+        this.isGeoHash = true;
+        this.geoHash = geoHash;
+    }
+    
+    public DSLGeoPoint(double latitude, double longitude) {
+        this(Mode.Properties, latitude, longitude);
+    }
+    
+    public DSLGeoPoint(Mode mode, double latitude, double longitude) {
+        this.mode = mode;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+    
+    public JsonElement getPointAsJsonElement() {
+        if(isGeoHash) {
+            return new JsonPrimitive(geoHash);
+        } else if(mode == Mode.Properties) {
+            JsonObject result = new JsonObject();
+            result.add(LAT, new JsonPrimitive(latitude));
+            result.add(LON, new JsonPrimitive(longitude));
+            return result;
+        } else if(mode == Mode.Array) {
+            JsonArray result = new JsonArray();
+            result.add(new JsonPrimitive(longitude));
+            result.add(new JsonPrimitive(latitude));
+            return result;
+        } 
+        // mode == Mode.LatLon
+        return new JsonPrimitive(latitude +", "+longitude);
+    }
+    
+}
