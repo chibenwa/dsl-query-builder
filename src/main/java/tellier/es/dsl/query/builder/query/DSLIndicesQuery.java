afee7ebb7495a805a4dbf2c377e6e3d71dc16938
benwa <btellier@linagora.com>
2015-02-06 15:14:12 +0100

    Indices query refactor

--- src/main/java/tellier/es/dsl/query/builder/query/DSLIndicesQuery.java
+++ src/main/java/tellier/es/dsl/query/builder/query/DSLIndicesQuery.java
@@ -17,17 +17,39 @@ public class DSLIndicesQuery implements DSLQuery {
     private final static String INDICES = "indices";
     private final static String QUERY = "query";
     private final static String NO_MATCH_QUERY = "no_match_query";
+    
+    public enum NoMatchMode {
+        NONE("none"),
+        ALL("all");
+        
+        private String tag;
+        
+        NoMatchMode(String tag) {
+            this.tag = tag;
+        }
+        
+        public String getTag() {
+            return tag;
+        }
+    }
 
     private List<String> indices = new ArrayList<String>();
     private DSLQuery query;
     private DSLQuery noMatchQuery;
+    private NoMatchMode noMatchMode;
 
     public DSLIndicesQuery(DSLQuery query) {
         this.query = query;
+        this.noMatchMode = NoMatchMode.ALL;
     }
 
     public DSLIndicesQuery setNoMatchQuery(DSLQuery noMatchQuery) {
         this.noMatchQuery = noMatchQuery;
+        return this;
+    }
+
+    public DSLIndicesQuery setNoMatchMode(NoMatchMode noMatchMode) {
+        this.noMatchMode = noMatchMode;
         return this;
     }
 
@@ -48,6 +70,8 @@ public class DSLIndicesQuery implements DSLQuery {
         indicesObject.add(QUERY, query.getQueryAsJson());
         if(noMatchQuery != null) {
             indicesObject.add(NO_MATCH_QUERY, noMatchQuery.getQueryAsJson());
+        } else if(noMatchMode != NoMatchMode.ALL) {
+            indicesObject.add(NO_MATCH_QUERY, new JsonPrimitive(noMatchMode.getTag()));
         }
         return result;
     }
